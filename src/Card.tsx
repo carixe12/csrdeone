67c3be16711aa928b5fc171225dcf2628cff67a2
claudio <user@Air-Polzovatel.sknt.ru>
2023-03-02 13:46:16 +0300

    pros

--- src/Card.tsx
+++ src/Card.tsx
@@ -4,12 +4,10 @@ import { CardComponent } from "./CardCompont";
 
 import { FC } from "react";
 interface IProps {
-  title1: string;
-  title2: string;
+  title: string;
   territorry: string;
   description: string;
-  description1: string;
-  description2: string;
+ 
 }
 
 export const Card: FC<IProps> = (props) => {
@@ -20,8 +18,7 @@ export const Card: FC<IProps> = (props) => {
           <div className="card">
             <img src={logo} className="App-logo" alt="logo" />
             <p className="logo"> Слой</p>
-            <h2 className="text1"> {props.title1} </h2>
-            <h2 className="text2"> {props.title2} </h2>
+            <h2 className="text1"> {props.title} </h2>
             <p className="text3"> {props.territorry} </p>
             <LocationOnIcon
               className="icon"
@@ -31,8 +28,7 @@ export const Card: FC<IProps> = (props) => {
               }}
             />
             <h4 className="text4"> {props.description} </h4>
-            <h4 className="text5"> {props.description1} </h4>
-            <h4 className="text6"> {props.description2} </h4>
+           
             <CardComponent schedual={{
               title: "4 квартал 2022",
               price: 28000,
